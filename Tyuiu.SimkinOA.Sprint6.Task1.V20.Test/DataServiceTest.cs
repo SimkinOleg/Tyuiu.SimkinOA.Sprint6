5a0bffe1a50cd4afcdb44a600c688ed4a9b7dc20
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-12-16 11:32:55 +0500

    Task1

---
+++ Tyuiu.SimkinOA.Sprint6.Task1.V20.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.SimkinOA.Sprint6.Task1.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.SimkinOA.Sprint6.Task1.V20/FormMain.cs
@@ -0,0 +1,15 @@
+namespace Tyuiu.SimkinOA.Sprint6.Task1.V20
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
