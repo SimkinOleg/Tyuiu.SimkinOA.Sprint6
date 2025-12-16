4cf6d1f437a520c8cda990602db0533ed87c17c5
Олег Симкин <Tyuiu.SimkinOA@outlook.com>
2025-12-16 14:05:08 +0500

    Task4

---
+++ Tyuiu.SimkinOA.Sprint6.Task4.V29/FormMain.cs
@@ -0,0 +1,15 @@
+namespace Tyuiu.SimkinOA.Sprint6.Task4.V29
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
